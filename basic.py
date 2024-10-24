e118576c1560921dd9f87ebda1af9c16e82e545c
Vikas10-coder <jakkevarunvikas@gmail.com>
2024-10-24 22:37:39 +0530

    basics.py

---
+++ basic.py
@@ -0,0 +1,19 @@
+# name="vikasjakke"
+ #age=22
+ #print("Name:", name)
+ #print("Age:", age)
+ 
+
+#x = 10
+# Reassign x to x + 5
+#x = x + 5
+#print(x)
+
+#user_input = input("Enter a number: ")
+
+# Convert the input to an integer
+#number = int(user_input)
+
+float=2.4
+int=float
+print(int)
